4e79c2b977a7d67583e4fe83544a1cfde33b403a
leosteil <leonardo.steil@objective.com.br>
2022-08-04 13:24:17 -0300

    poc

--- src/CaptureHandler.php
+++ src/CaptureHandler.php
@@ -2,15 +2,10 @@
 
 namespace POC;
 
-use EBANX\API\Capture\CaptureRequestCaller;
-use EBANX\API\Capture\CaptureRequestCallerParams;
-
 class CaptureHandler
 {
     public function execute(CaptureCommand $capture)
     {
-        $captureData = new CaptureRequestCallerParams($capture->getIntegrationKey(), $capture->getPaymentHash(), '');
-
-        return CaptureRequestCaller::executeCapture($captureData);
+        //calls web service
     }
 }
